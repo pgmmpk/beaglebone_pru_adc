75acdb7b47a7a723621bb60476ebb624e6c0b30e
Mike Kroutikov <pgmmpk@gmail.com>
2014-03-08 15:34:52 -0500

    cleanup

--- beaglebone_pru_adc/__init__.py
+++ beaglebone_pru_adc/__init__.py
@@ -43,6 +43,20 @@ def _ensure_adc_loaded():
 _ensure_pru_loaded()
 _ensure_adc_loaded()
 
+# Useful offsets from firmware.h
+OFF_FLAG        = 0x0008
+OFF_TIMER       = 0x0004
+OFF_EMA_POW     = 0x001c
+OFF_VALUES      = 0x0020
+OFF_ENC0_PIN    = 0x0040
+OFF_ENC1_PIN    = 0x0041
+OFF_ENC0_THRESH = 0x0044
+OFF_ENC1_THRESH = 0x0064
+OFF_ENC0_VALUES = 0x0048
+OFF_ENC1_VALUES = 0x0068
+OFF_SCOPE_ADDR  = 0x000c
+OFF_SCOPE_OFFSET= 0x0010
+OFF_SCOPE_SIZE  = 0x0014
 class Capture(_pru_adc.Capture):
     
 	def __init__(self):
@@ -73,7 +87,7 @@ class Capture(_pru_adc.Capture):
 		_pru_adc.Capture.start(self, firmware)
 	
 	def stop(self):
-		self._set_word(0x0008, 1) # exit flag
+		self._set_word(OFF_FLAG, 1) # exit flag
     
 	def close(self):
 		_pru_adc.Capture.close(self)
@@ -85,65 +99,65 @@ class Capture(_pru_adc.Capture):
 		"""
             Returns ADC timer value. This is the number of ADC capture cycles since driver start
             """
-		return self._get_word(0x0004)
+		return self._get_word(OFF_TIMER)
 	
 	@property
 	def ema_pow(self):
 		"""
             Returns EMA exponent. If zero, no EMA averaging
             """
-		return self._get_word(0x001c)
+		return self._get_word(OFF_EMA_POW)
 	
 	@ema_pow.setter
 	def ema_pow(self, value):
 		if 0 <= value <= 31:
-			self._set_word(0x001c, value)
+			self._set_word(OFF_EMA_POW, value)
 		else:
 			raise ValueError("ema_pow must be in range 0-31")
 	
 	@property
 	def values(self):
-		return struct.unpack("LLLLLLLL", self._mem[0x0020:0x0020+8*4])
+		return struct.unpack("LLLLLLLL", self._mem[OFF_VALUES:OFF_VALUES+8*4])
 	
 	@property
 	def encoder0_pin(self):
-		return struct.unpack("b", self._mem[0x0040:0x0040+1])[0]
+		return struct.unpack("b", self._mem[OFF_ENC0_PIN:OFF_ENC0_PIN+1])[0]
 	
 	@encoder0_pin.setter
 	def encoder0_pin(self, value):
-		struct.pack_into('b', self._mem, 0x0040, value)
+		struct.pack_into('b', self._mem, OFF_ENC0_PIN, value)
 	
 	@property
 	def encoder1_pin(self):
-		return struct.unpack("b", self._mem[0x0041:0x0041+1])[0]
+		return struct.unpack("b", self._mem[OFF_ENC1_PIN:OFF_ENC1_PIN+1])[0]
 	
 	@encoder1_pin.setter
 	def encoder1_pin(self, value):
-		struct.pack_into('b', self._mem, 0x0041, value)
+		struct.pack_into('b', self._mem, OFF_ENC1_PIN, value)
 	
 	@property
 	def encoder0_threshold(self):
-		return self._get_word(0x44)
+		return self._get_word(OFF_ENC0_THRESH)
 	
 	@encoder0_threshold.setter
 	def encoder0_threshold(self, value):
-		self._set_word(0x44, value)
+		self._set_word(OFF_ENC0_THRESH, value)
     
 	@property
 	def encoder1_threshold(self):
-		return self._get_word(0x64)
+		return self._get_word(OFF_ENC1_THRESH)
     
 	@encoder1_threshold.setter
 	def encoder1_threshold(self, value):
-		self._set_word(0x64, value)
+		self._set_word(OFF_ENC1_THRESH, value)
     
 	@property
 	def encoder0_values(self):
-		return struct.unpack("LLLLL", self._mem[0x0048:0x0048+5*4])
+		return struct.unpack("LLLLL", self._mem[OFF_ENC0_VALUES:OFF_ENC0_VALUES+5*4])
 	
 	@property
 	def encoder1_values(self):
-		return struct.unpack("LLLLL", self._mem[0x0068:0x0068+5*4])
+		return struct.unpack("LLLLL", self._mem[OFF_ENC1_VALUES:OFF_ENC1_VALUES+5*4])
     
 	def _set_word(self, byte_offset, value):
 		struct.pack_into('L', self._mem, byte_offset, value)
@@ -154,9 +168,9 @@ class Capture(_pru_adc.Capture):
 	def oscilloscope_capture_init(self, offset, numsamples):
 		if numsamples * 4 > self._ddr_size:
 			raise ValueError("numsamples is too large. Limit is (determined by DDR memory size): " + str(self._ddr_size//4))
-		self._set_word(0x0c, self._ddr_addr)
-		self._set_word(0x10, offset)
-		self._set_word(0x14, numsamples * 4)
+		self._set_word(OFF_SCOPE_ADDR, self._ddr_addr)
+		self._set_word(OFF_SCOPE_OFFSET, offset)
+		self._set_word(OFF_SCOPE_SIZE, numsamples * 4)
     
 	def oscilloscope_capture_complete(self):
 		return self._get_word(0x14) == 0
@@ -174,10 +188,3 @@ class Capture(_pru_adc.Capture):
 				out.append(struct.unpack("L", ddr[ddr_offset+i*4:ddr_offset+i*4+4])[0])
 			ddr.close()
 		return out
-
-def workaround_mmap_bug(addr):
-	print hex(addr)
-	if addr > 0x7fffffff:
-		print -1 - addr
-		return 0x7fffffffff - addr
-	return addr
