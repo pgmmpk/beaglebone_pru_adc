e866cfdb9066b4d98571362d9851c9ebae9a68d1
Mike Kroutikov <pgmmpk@gmail.com>
2014-03-12 16:33:07 -0400

    better examples

--- examples/speed.py
+++ examples/speed.py
@@ -1,18 +1,25 @@
 import beaglebone_pru_adc as adc
 
+print 'Will start capture, keep it running for 10 seconds, then get timer value'
+print 'This is used to compute the capture speed (time value of one timer unit)'
+
 capture = adc.Capture()
+
 capture.encoder0_pin = 0 # AIN0, aka P9_39
 capture.encoder1_pin = 2 # AIN2, aka P9_37
 capture.encoder0_threshold = 3000 # you will want to adjust this
 capture.encoder1_thredhold = 3000 # and this...
 capture.encoder0_delay = 100 # prevents "ringing", adjust if needed
 capture.encoder1_delay = 100 # ... same
-
 capture.start()
 
-for _ in range(1000):
-    print capture.timer, capture.encoder0_values, capture.encoder1_values
+time.sleep(10)
+
+timer = capture.timer
 
 capture.stop()
 capture.wait()
 capture.close()
+
+print 'Capture runs at %4.2f readings per second' % timer / 10.
+print 'Time value of one timer unit is %d nanosec' % 1000000000 // timer
