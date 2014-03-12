1e433be19c23b7b00eb4ed8d287cd68859384f64
root <root@beaglebone.(none)>
2014-03-12 19:58:59 +0000

    final touches on delay feature

--- examples/encoders_test.py
+++ examples/encoders_test.py
@@ -3,13 +3,15 @@ import contextlib
 import time
 
 @contextlib.contextmanager
-def init_capture(threshold0, threshold1):
+def init_capture(threshold0, threshold1, delay=0):
 
 	capture = adc.Capture()
 	capture.encoder0_pin = 0 # AIN0, aka P9_39
 	capture.encoder1_pin = 2 # AIN2, aka P9_37
 	capture.encoder0_threshold = threshold0
 	capture.encoder1_thredhold = threshold1
+	capture.encoder0_delay = delay
+	capture.encoder1_delay = delay
 	capture.start()
 
 	yield capture
@@ -18,7 +20,7 @@ def init_capture(threshold0, threshold1):
 	capture.wait()
 	capture.close()
 
-with init_capture(2952, 2475) as c:
+with init_capture(2000, 2000, 200) as c:
 
 	enc0_ticks = c.encoder0_values[3]
 	enc1_ticks = c.encoder1_values[3]
@@ -30,8 +32,8 @@ with init_capture(2952, 2475) as c:
 		if v0[3] != enc0_ticks or v1[3] != enc1_ticks:
 			enc0_ticks = v0[3]
 			enc1_ticks = v1[3]
-			enc0_speed = 1000. / v0[4]
-			enc1_speed = 1000. / v1[4]
+			enc0_speed = 10000. / v0[4]
+			enc1_speed = 10000. / v1[4]
 
 			print '%8d[%4.2lf] %8d[%4.2lf]' % (enc0_ticks, enc0_speed, enc1_ticks, enc1_speed)
 
